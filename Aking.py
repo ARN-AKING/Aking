5fc45cac52737a892b9d6e27a3cfa1e51f9a47e0
ARN - AKING <120348223+ARN-AKING@users.noreply.github.com>
2023-11-30 19:42:55 +0430

    Update Aking.py

--- Aking.py
+++ Aking.py
@@ -4,27 +4,27 @@ def xoss(z):
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.02)
-xoss('\n\x1b[1;37m[◇] Checking Update....');time.sleep(0.5)
+xoss('\n\x1b[1;37m[☆] Checking Update....');time.sleep(0.5)
 def Update():
-    xoss('\033[1;31m[◇] Commands On Update Please Wait For Update ');exit()
+    xoss('\033[1;31m[+] Commands On Update Please Wait For Update ');exit()
 def Run():
-        bit = platform.architecture()[0]
-        if bit == '64bit':
+        AKING = platform.architecture()[0]
+        if AKING == '64bit':
             Update()
-            xoss("\x1b[1;92m[◇] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
-            xoss("\x1b[1;92m[◇] Your Device 64 BIT 💥");time.sleep(1)
-            xoss("\x1b[1;92m[◇] FOLLOW MY FACEBOOK ACCOUNT");time.sleep(1)
+            xoss("\x1b[1;92m[☆] Congratulations : Your Device Support this Tools");time.sleep(1)
+            xoss("\x1b[1;92m[☆] Your Device 64 BIT");time.sleep(1)
+            xoss("\x1b[1;92m[☆] FOLLOW MY FACEBOOK ACCOUNT");time.sleep(1)
             os.system("xdg-open https://www.facebook.com/Rayees.Amir4201/");Update()
             import AKING64
 
-        elif bit == '32bit':
+        elif AKING == '32bit':
             Update()
-            xoss("\x1b[1;92m[◇] Congratulations ! Your Device Support this Tools 🙂");time.sleep(1)
-            xoss("\x1b[1;92m[◇] Your Device 32 BIT 💥");time.sleep(1)
-            xoss("\x1b[1;92m[◇] FOLLOW MY FACEBOOK ACCOUNT");time.sleep(1)
+            xoss("\x1b[1;92m[☆] Congratulations : Your Device Support this Tools");time.sleep(1)
+            xoss("\x1b[1;92m[☆] Your Device 32 BIT");time.sleep(1)
+            xoss("\x1b[1;92m[☆] FOLLOW MY FACEBOOK ACCOUNT");time.sleep(1)
             os.system("xdg-open https://www.facebook.com/Rayees.Amir4201/")
             import AKING32
             print(50*"-")
         else:
-            exit('\033[1;31m[●] Connection & Network Error 🤕')
+            exit('\033[1;31m[+] Connection & Network Error')
 Run()
